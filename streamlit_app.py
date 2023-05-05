9da004e632e6e1047dfd6344b3e6d88223d29297
whitson-buck <whitsonbuck@gmail.com>
2023-05-04 22:28:18 -0400

    puuuuuuuuush

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,10 +26,10 @@ st.markdown(
 st.header("Instructions")
 
 markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+1. Step one
+2. Step two
+3. Step three
+4. Step four
 
 """
 
