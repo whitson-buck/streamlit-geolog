d97be7d3fb59426244a21ff079f34280665f9d72
whitson-buck <whitsonbuck@gmail.com>
2023-05-05 16:34:02 -0400

    pushing

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,11 @@ st.set_page_config(layout="wide")
 
 # Customize the sidebar
 markdown = """
-(change this) Web App URL: <https://geotemplate.streamlit.app>
+Web App URL: <https://geolog.streamlit.app>
 GitHub Repository: <https://github.com/whitson-buck/geolog>
 """
 
-st.sidebar.title("About")
+st.sidebar.title("Globe!")
 st.sidebar.info(markdown)
 logo = "https://i.pinimg.com/originals/0b/f2/85/0bf2854f6e017a49d461c719402425dc.gif"
 st.sidebar.image(logo)
