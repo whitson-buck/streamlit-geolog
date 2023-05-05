75dc41a0aa45c39c9faef8ab506519bfa7caf075
Whitty <67715134+whitson-buck@users.noreply.github.com>
2023-05-05 15:25:18 -0400

    Update 7_🍗_lab10Geolog.py

--- pages/7_🍗_lab10Geolog.py
+++ pages/7_🍗_lab10Geolog.py
@@ -13,9 +13,7 @@ index= options.index("OpenTopoMap")
 with col2:
     dropdown = st.selectbox("Basemap", options, index)
 
-    default_url = leafmap.basemaps[dropdown].tiles
-
-    url = st.text_input("Enter URL", default_url)
+    url = st.text_input("Enter URL")
 
 with col1:
     m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
